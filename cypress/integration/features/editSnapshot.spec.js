c78a4db8fce56cafba923d5b160e7f23eea600db
Riccardo Noviello <riccardo.noviello@gmail.com>
2020-08-13 16:46:32 +0100

    amend failing test

--- cypress/integration/features/editSnapshot.spec.js
+++ cypress/integration/features/editSnapshot.spec.js
@@ -7,7 +7,7 @@ context('Edit snapshot', () => {
       queryLastName: 'flynn',
       assets: [],
       createdBy: 'Dat',
-      systemIds: ['wub'],
+      systemIds: ['inh-123'],
       created: '2019-06-09T15:46:47.857Z',
       dob: '2000-06-09',
       vulnerabilities: [],
