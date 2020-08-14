836214785620b7b71694503daec42dd51ff3680f
Riccardo Noviello (Future Gov) <63246336+rik86future@users.noreply.github.com>
2020-08-14 13:19:46 +0100

    add hotjar and feeback google form btn (#5)
    * add hotjar and feeback google form btn
    
    * externalise the hotjar id
    
    Co-authored-by: Riccardo Noviello <riccardo.noviello@gmail.com>

--- next.config.js
+++ next.config.js
@@ -5,7 +5,8 @@ module.exports = {
   },
   env: {
     ADDRESSES_API_URL: process.env.ADDRESSES_API_URL,
-    ADDRESSES_API_KEY: process.env.ADDRESSES_API_KEY
+    ADDRESSES_API_KEY: process.env.ADDRESSES_API_KEY,
+    HOTJAR_ID: process.env.HOTJAR_ID
   },
   distDir: 'build/_next',
   target: 'server'
