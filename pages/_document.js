7f17ab4b9d5b44fd79fed3cb0b937e2eb8b9a4c3
Riccardo Noviello <riccardo.noviello@gmail.com>
2020-08-14 10:26:47 +0100

    externalise the hotjar id

--- pages/_document.js
+++ pages/_document.js
@@ -1,6 +1,7 @@
 import Document, { Html, Head, Main, NextScript } from 'next/document';
 
 export default class AppDocument extends Document {
+
   render() {
     return (
       <Html className="govuk-template lbh-template">
@@ -8,7 +9,7 @@ export default class AppDocument extends Document {
         <script dangerouslySetInnerHTML={{ __html: `
             (function(h,o,t,j,a,r){
                 h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
-                h._hjSettings={hjid:1944227,hjsv:6};
+                h._hjSettings={hjid:${process.env.HOTJAR_ID},hjsv:6};
                 a=o.getElementsByTagName('head')[0];
                 r=o.createElement('script');r.async=1;
                 r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
