c9c55465d28b8d06534239f4b0d00dd66c7b5d35
Kyle <kjdchapman@gmail.com>
2020-09-01 10:26:44 +0100

    Add failing test for searching for tags
    Notably, this commit uses a strategy of rendering directly rather
    than assigning it to a container. I'm not familiar with
    testing-library, so I'm starting with the simplest thing that works.

--- components/Feature/TopicExplorer/index.test.js
+++ components/Feature/TopicExplorer/index.test.js
@@ -1,8 +1,33 @@
-import { render, screen } from '@testing-library/react';
+import { render, screen, fireEvent } from '@testing-library/react';
 import TopicExplorer from './index';
 
 describe('TopicExplorer', () => {
-  it('renders successfully', () => {
-    render(<TopicExplorer />);
-  })
+  describe('renders successfully with props', () => {
+    var topics = [
+      { prompt: 'topic one', tags: ['one, 1, first'] },
+      { prompt: 'topic two', tags: ['two, 2, second'] },
+    ]
+
+    render(<TopicExplorer topics={topics}/>);
+
+    it('renders successfully', () => {
+      expect(screen.getByText('Conversational prompts')).toBeInTheDocument();
+    });
+
+    describe('with a search for topic one', () => {
+      fireEvent.change(screen.getByRole('textbox'), {
+        target: { value: 'one' },
+      });
+
+      expect(screen.getByText('topic one')).toBeInTheDocument
+    });
+
+    xdescribe('with a search for topic two', () => {
+      fireEvent.change(screen.getByRole('textbox'), {
+        target: { value: 'two' },
+      });
+
+      expect(screen.getByText('topic two')).toBeInTheDocument
+    });
+  });
 })
