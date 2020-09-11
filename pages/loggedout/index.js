c474f6c01376a7b64398be3ce6a5c65759424bac
Kyle <kjdchapman@gmail.com>
2020-09-11 10:08:35 +0100

    Add more to 403 ("you can't access this") page
    This commit tries to make the next steps more obvious for someone who
    arrives at the snapshot but isn't logged in.
    
    I believe this NEXT_PUBLIC env var should be available in all
    environments already.

--- pages/loggedout/index.js
+++ pages/loggedout/index.js
@@ -1,5 +1,11 @@
 export default function LoggedOut() {
+  const returnUrl = "https://auth.hackney.gov.uk/auth?redirect_uri=" + process.env.NEXT_PUBLIC_URL
+
   return (
-    <div>You are not currently logged in. Please login via Support For Hackney residents.</div>
+    <>
+      <h1 className='govuk-heading-l'>You need permission to access this service</h1>
+      <p className='govuk-body'><a href={returnUrl} className='govuk-link'>Log in to your Hackney account.</a></p>
+      <p className='govuk-body'>If you're already logged in, contact Chris Caden to get access (christopher.caden@hackney.gov.uk).</p>
+    </>
   );
 }
