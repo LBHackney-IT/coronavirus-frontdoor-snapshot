425cb9af1262edc7a7971ea565bf18210f1e0459
Kyle <kjdchapman@gmail.com>
2020-09-29 12:10:18 +0100

    Use _document tag for GA tracking code
    Inspired by the implementation at https://github.com/vercel/next.js/tree/
    canary/examples/with-google-analytics.
    
    This commit also removes the existing tag manager code, which wasn't working
    with Hackney's current GA setup.

--- pages/_document.js
+++ pages/_document.js
@@ -6,16 +6,32 @@ export default class AppDocument extends Document {
     return (
       <Html className="govuk-template lbh-template">
         <Head>
-        <script dangerouslySetInnerHTML={{ __html: `
+          <script dangerouslySetInnerHTML={{ __html: `
             (function(h,o,t,j,a,r){
-                h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
-                h._hjSettings={hjid:${process.env.HOTJAR_ID},hjsv:6};
-                a=o.getElementsByTagName('head')[0];
-                r=o.createElement('script');r.async=1;
-                r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
-                a.appendChild(r);
-            })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
-            `}} />
+              h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
+              h._hjSettings={hjid:${process.env.HOTJAR_ID},hjsv:6};
+              a=o.getElementsByTagName('head')[0];
+              r=o.createElement('script');r.async=1;
+              r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
+              a.appendChild(r);
+             })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
+          `}} />
+          <script
+            async
+            src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GTM_ID}`}
+          />
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${process.env.NEXT_PUBLIC_GTM_ID}', {
+              page_path: window.location.pathname,
+            });
+          `,
+            }}
+          />
         </Head>
         <body className="govuk-template__body lbh-template__body js-enabled">
           <Main />
