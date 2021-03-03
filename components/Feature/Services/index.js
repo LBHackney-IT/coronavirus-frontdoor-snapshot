e18c6890c16bb045478f887206baf982f85aac2d
Kat <katrina@madetech.com>
2021-03-03 14:37:33 +0000

    Add services feature component and replace vulnerabilities grid component with Services on the home page

---
+++ components/Feature/Services/index.js
@@ -0,0 +1,55 @@
+import { useState } from 'react';
+import ResourceCard from 'components/Feature/VulnerabilitiesGrid/ResourceCard';
+import {
+  Accordion,
+  AccordionItem
+} from 'components/Form';
+
+const Services = ({ resources, taxonomies }) => {
+  const [expandedGroups, setExpandedGroups] = useState({});
+
+  return (
+    <>
+      <div className="govuk-grid-column-full-width">
+        <Accordion
+          title=""
+        >
+          {taxonomies.map(taxonomy => {
+            return (
+              <>
+                <AccordionItem
+                  key={taxonomy.id}
+                  id={taxonomy.id}
+                  heading={taxonomy.name}
+                  onClick={expanded =>
+                    setExpandedGroups({
+                      ...expandedGroups,
+                      [taxonomy.id]:
+                        expandedGroups[taxonomy.id] !== undefined
+                          ? !expandedGroups[taxonomy.id]
+                          : expanded
+                    })
+                  }
+                >
+                  {resources.map(
+                    resource =>
+                      taxonomy.name == resource.categoryName && (
+                        <ResourceCard
+                          key={resource.id}
+                          data-testid={`resource-${resource.id}`}
+                          {...resource}
+                          updateSelectedResources={() => {}}
+                        />
+                      )
+                  )}
+                </AccordionItem>
+              </>
+            );
+          })}
+        </Accordion>
+      </div>
+    </>
+  );
+};
+
+export default Services;
