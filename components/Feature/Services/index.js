e4f2620b572b203aa3bfdf81e195ee1143cbc4de
Kat <katrina@madetech.com>
2021-03-08 11:52:28 +0000

    Filter accordion items to only display categories that contain services

--- components/Feature/Services/index.js
+++ components/Feature/Services/index.js
@@ -1,48 +1,50 @@
 import { useState } from 'react';
 import ResourceCard from 'components/Feature/VulnerabilitiesGrid/ResourceCard';
-import {
-  Accordion,
-  AccordionItem
-} from 'components/Form';
+import { Accordion, AccordionItem } from 'components/Form';
 
 const Services = ({ resources, taxonomies }) => {
   const [expandedGroups, setExpandedGroups] = useState({});
 
+  taxonomies.forEach(
+    taxonomy =>
+      (taxonomy.resources = resources
+        .map(resource => {
+          if (taxonomy.name == resource.categoryName) return resource;
+        })
+        .filter(x => x))
+  );
   return (
     <>
       <div className="govuk-grid-column-full-width">
-        <Accordion
-          title=""
-        >
+        <Accordion title="">
           {taxonomies.map(taxonomy => {
             return (
               <>
-                <AccordionItem
-                  key={`taxonomy-${taxonomy.id}`}
-                  id={taxonomy.id}
-                  heading={taxonomy.name}
-                  onClick={expanded =>
-                    setExpandedGroups({
-                      ...expandedGroups,
-                      [taxonomy.id]:
-                        expandedGroups[taxonomy.id] !== undefined
-                          ? !expandedGroups[taxonomy.id]
-                          : expanded
-                    })
-                  }
-                >
-                  {resources.map(
-                    resource =>
-                      taxonomy.name == resource.categoryName && (
-                        <ResourceCard
-                          key={`resource-card-${resource.id}-${resource.name}`}
-                          data-testid={`resource-${resource.id}`}
-                          {...resource}
-                          updateSelectedResources={() => {}}
-                        />
-                      )
-                  )}
-                </AccordionItem>
+                {taxonomy.resources.length > 0 && (
+                  <AccordionItem
+                    key={`taxonomy-${taxonomy.id}`}
+                    id={taxonomy.id}
+                    heading={taxonomy.name}
+                    onClick={expanded =>
+                      setExpandedGroups({
+                        ...expandedGroups,
+                        [taxonomy.id]:
+                          expandedGroups[taxonomy.id] !== undefined
+                            ? !expandedGroups[taxonomy.id]
+                            : expanded
+                      })
+                    }
+                  >
+                    {taxonomy.resources.map(resource => (
+                      <ResourceCard
+                        key={`resource-card-${resource.id}-${resource.name}`}
+                        data-testid={`resource-${resource.id}`}
+                        {...resource}
+                        updateSelectedResources={() => {}}
+                      />
+                    ))}
+                  </AccordionItem>
+                )}
               </>
             );
           })}
