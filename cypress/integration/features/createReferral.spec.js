8263353c62be68620c7a86b568b111015c393be1
Kat <katrina@madetech.com>
2021-03-22 14:31:59 +0000

    Add send referral cypress tests

--- cypress/integration/features/createReferral.spec.js
+++ cypress/integration/features/createReferral.spec.js
@@ -8,7 +8,7 @@ context('Create referral', () => {
 
     it('resident form is hidden', () => {
       cy.contains('Residents details').should('be.visible');
-      cy.contains('#resident-details').should('not.be.visible');
+      cy.get('#resident-details').should('not.be.visible');
     });
 
     it('referral form', () => {
@@ -81,9 +81,95 @@ context('Create referral', () => {
       cy.get('#referer-email-ABC123').should('have.class', 'govuk-input--error');
     });
   });
+
+  describe('Referral form', () => {
+    before(() => {
+      cy.visit('/');
+      cy.get('[data-testid=accordion-item]')
+        .eq(0)
+        .click();
+    });
+    it('Collapses other referral forms if a new form is expanded', () => {
+      cy.get('#referral-ABC123-form').should('not.exist');
+
+      cy.get('#referral-ABC123').click();
+      cy.get('#referral-ABC123-form').should('exist');
+
+      cy.get('#referral-abc').click();
+
+      cy.get('#referral-ABC123-form').should('not.exist');
+    });
+    it('Persists referral information across different referral fields', () => {
+      cy.get('#referral-ABC123').click();
+
+      cy.get('#referral-reason-ABC123').type('First referral reason value');
+      cy.get('#conversation-notes-ABC123').type('First conversation notes value');
+
+      cy.get('#referral-abc').click();
+      cy.get('#referral-reason-abc').should('have.value', 'First referral reason value');
+      cy.get('#conversation-notes-abc').should('have.value', 'First conversation notes value');
+    });
+    it('Persists referrer information across different referral fields', () => {
+      cy.get('#referral-ABC123').click();
+
+      cy.get('#referer-name-ABC123').type('Tina Belcher');
+      cy.get('#referer-organisation-ABC123').type("Bob's Burgers");
+      cy.get('#referer-email-ABC123').type('perm-fail@simulator.notify');
+
+      cy.get('#referral-abc').click();
+      cy.get('#referer-name-abc').should('have.value', 'Tina Belcher');
+      cy.get('#referer-organisation-abc').should('have.value', "Bob's Burgers");
+      cy.get('#referer-email-abc').should('have.value', 'perm-fail@simulator.notify');
+    });
+  });
+  describe('Make a referral', () => {
+    beforeEach(() => {
+      cy.visit('/');
+      cy.get('[data-testid=accordion-item]')
+        .eq(0)
+        .click();
+      cy.get('#referral-ABC123').click();
+      cy.get('#firstName').type('Luna');
+      cy.get('#lastName').type('Kitty');
+      cy.get('#phone').type('07700900000');
+      cy.get('#email').type('luna@meow.com');
+      cy.get('#address').type('159 Cute Street');
+      cy.get('#postcode').type('M3 0W');
+
+      cy.get('#referral-reason-ABC123').type(
+        'Sunt in culpa qui officia deserunt mollit anim id est laborum.'
+      );
+      cy.get('#conversation-notes-ABC123').type('Excepteur sint occaecat cupidatat non proident');
+      cy.get('#consent-ABC123').click();
+      cy.get('#referer-name-ABC123').type('Tina Belcher');
+      cy.get('#referer-organisation-ABC123').type("Bob's Burgers");
+    });
+    it('Displays a success message if referral is made', () => {
+      cy.intercept('/api/referrals', {
+        status: 201,
+        body: {
+          id: '123',
+          errors: []
+        }
+      });
+      cy.get('#referer-email-ABC123').type('simulate-delivered@test.com');
+      cy.get('#submit-ABC123').click();
+      cy.get(`[data-testid=successful-referral-banner]`).should('be.visible');
+    });
+
+    it('Displays error message if refferal fails', () => {
+      cy.intercept('/api/referrals', {
+        status: 201,
+        body: {
+          id: '123',
+          errors: ['error']
+        }
+      });
+      cy.get('#referer-email-ABC123').type('perm-fail@test.com');
+      cy.get('#submit-ABC123').click();
+      cy.get(`[data-testid=referral-errors-banner]`).should('be.visible');
+    });
+  });
 });
 
-
 // test that not all services have refer button
-// test green box
-// test and implemnt red box
