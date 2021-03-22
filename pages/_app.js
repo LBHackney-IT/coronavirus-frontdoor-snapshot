ecff3268b3935ee399d48807808e2ee6d511529a
Kat <katrina@madetech.com>
2021-03-22 10:57:38 +0000

    Don't display error message in test environment

--- pages/_app.js
+++ pages/_app.js
@@ -15,7 +15,7 @@ export default class MyApp extends App {
           <Component {...pageProps} />
         </Layout>
         <script src="/js/govuk.js"></script>
-        <script src="/js/beforeUnload.js"></script>
+        {process.env.NEXT_PUBLIC_ENV != 'test' && <script src="/js/beforeUnload.js"></script>}
       </>
     );
   }
