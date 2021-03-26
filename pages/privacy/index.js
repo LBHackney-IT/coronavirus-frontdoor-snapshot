7be15f88925cf8b46cb9d6ae3415597c09d81b7d
Kat <katrina@madetech.com>
2021-03-26 11:30:06 +0000

    Add privacy page

---
+++ pages/privacy/index.js
@@ -0,0 +1,78 @@
+import Heading from 'components/Heading';
+export default function Privacy() {
+  return (
+    <>
+      <Heading as="h1">Better Conversations Privacy Statement</Heading>
+      <Heading as="h2">Analytics and cookies</Heading>
+      <p>
+        We use Google Analytics and Hotjar to collect information about how you use this site. We do
+        this to make sure it’s meeting your needs and to understand how we can make the product work
+        better.
+      </p>
+      <p>
+        Google Analytics stores information about what pages on this site you visit, how long you
+        are on the site, how you got here and what you click on while you are here.
+      </p>
+      <p>
+        Hotjar sets cookies to help us track behaviour across pages and to control visitor polls.
+        The cookies carry no personally identifiable information.
+      </p>
+      <p>
+        We do not use either Hotjar or Google Analytics to collect personal information so this data
+        cannot be used to identify who you are. Please read the full Hackney Privacy statement for
+        further information.
+      </p>
+      <p>
+        Cookies are small anonymous text files that are placed on your computer by websites that you
+        visit. The Council is committed to only using cookies that are either essential (ie they are
+        required to make something work) or that help us to make your experience of using the web
+        site better.
+      </p>
+      <Heading as="h2">Change cookie settings for this website</Heading>
+      <p>
+        Most web browsers allow some control of cookies through the browser settings.{' '}
+        <a href="http://www.allaboutcookies.org/">Find out more about cookies</a>, including how to
+        see what cookies have been set and how to manage and delete them.
+      </p>
+      <ul>
+        <li>
+          <p>
+            <a href="https://tools.google.com/dlpage/gaoptout">
+              opt out of being tracked by Google Analytics
+            </a>{' '}
+            across all websites
+          </p>
+        </li>
+        <li>
+          <p>
+            <a href="https://www.hotjar.com/legal/compliance/opt-out">
+              opt out of being tracked by Hotjar
+            </a>
+          </p>
+        </li>
+      </ul>
+      <h2>Who to contact about data protection</h2>
+      <p>
+        Hackney Council is a data controller, registered with the Information Commissioners Office.
+      </p>
+      <p>
+        You can email our Data Protection Officer if you have any queries about your data
+        dataprotection@hackney.gov.uk.
+      </p>
+      <p>
+        For independent advice about data protection, privacy and data sharing issues, you can
+        contact the Information Commissioners Office (ICO) at:
+      </p>
+      <p>Information Commissioners Office</p>
+      <p>Wycliffe House</p>
+      <p>Water Lane</p>
+      <p>Wilmslow</p>
+      <p>Cheshire</p>
+      <p>SK9 5AF</p>
+      <p>Tel: 0303 123 1113 (local rate) or 01625 545 745 (national rate)</p>
+      <p>
+        Or you can visit <a href="http://ico.org.uk/">ico.org.uk</a> or email casework@ico.org.uk
+      </p>
+    </>
+  );
+}
