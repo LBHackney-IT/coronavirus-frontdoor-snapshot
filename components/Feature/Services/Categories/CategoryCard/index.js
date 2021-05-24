c621a7a5f0b97be008408ee2b9d97ab2c9e3c237
Kat <katrina@madetech.com>
2021-05-24 11:20:00 +0100

    Map colors for category icons

--- components/Feature/Services/Categories/CategoryCard/index.js
+++ components/Feature/Services/Categories/CategoryCard/index.js
@@ -1,7 +1,6 @@
 // import React, { useContext } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import {
-  faPhone,
   faComments,
   faCircle,
   faSquare,
@@ -20,28 +19,38 @@ import {
 import styles from './index.module.scss';
 const CategoryCard = ({ category, onclick }) => {
   const categoryIcons = {
-    'First category': faPhone,
-    'Second category': faComments,
-    'Loneliness or isolation': faComments,
-    'Anxiety or mental health': faHeadSideVirus,
-    'Safe and healthy body': faHeartbeat,
-    'Exercise and wellbeing': faWalking,
-    'Arts and creativity': faPalette,
-    'Food or shopping': faAppleAlt,
-    'Faith-led activities': faHandHoldingHeart,
-    'Money advice': faPoundSign,
-    'Employment advice': faSuitcase,
-    'Housing advice': faHome,
-    'Immigration advice': faInfo,
-    'Disabilities or autism': '',
-    default: faSquare
+    'Loneliness or isolation': { icon: faComments, color: '#df1995' },
+    'Anxiety or mental health': { icon: faHeadSideVirus, color: '#ff6a13' },
+    'Safe and healthy body': { icon: faHeartbeat, color: '#84bd00' },
+    'Exercise and wellbeing': { icon: faWalking, color: '#e03c31' },
+    'Arts and creativity': { icon: faPalette, color: '#025ea6' },
+    'Food or shopping': { icon: faAppleAlt, color: '#328472' },
+    'Faith-led activities': { icon: faHandHoldingHeart, color: '#0085ca' },
+    'Money advice': { icon: faPoundSign, color: '#81312f' },
+    'Employment advice': { icon: faSuitcase, color: '#8031a7' },
+    'Housing advice': { icon: faHome, color: '#2b8cc4' },
+    'Immigration advice': { icon: faInfo, color: '#00664f' },
+    'Disabilities or autism': { icon: '', color: '#' },
+    default: { icon: faSquare, color: '#df1995' }
   };
+
   return (
     <div className={`${styles['card']}`} onClick={() => onclick(category.name)}>
       <div className={`${styles['icon']} fa-layers fa-fw`}>
-        <FontAwesomeIcon icon={faCircle} color="green" />
         <FontAwesomeIcon
-          icon={categoryIcons[category.name] ? categoryIcons[category.name] : categoryIcons.default}
+          icon={faCircle}
+          color={
+            categoryIcons[category.name]
+              ? categoryIcons[category.name].color
+              : categoryIcons.default.color
+          }
+        />
+        <FontAwesomeIcon
+          icon={
+            categoryIcons[category.name]
+              ? categoryIcons[category.name].icon
+              : categoryIcons.default.icon
+          }
           inverse
           transform="shrink-6"
         />
