9a99652652254415d422e239e3e59dea0900083d
owenpigott <80220779+owenpigott@users.noreply.github.com>
2021-06-17 14:19:24 +0100

    Added feedback form beneath search results (#111)
    * Added feedback form beneath search results
    
    * Feedback form now shows under all conditions (Category select or search results)
    
    * Moved values from DOM access to state. This highlighted an issue where Category Card title wasn't being sent with search feedback. Also fixed.
    
    Co-authored-by: Owen Pigott <owen.pigott@madetech.com>

--- components/Feature/Services/index.js
+++ components/Feature/Services/index.js
@@ -1,10 +1,15 @@
 import { useState } from 'react';
 import ResourceCard from 'components/Feature/VulnerabilitiesGrid/ResourceCard';
 import Categories from './Categories';
+import Details from 'components/Form/Details';
 import styles from './index.module.scss';
 import { sendDataToAnalytics, getUserGroup } from 'lib/utils/analytics';
 import { getSearchResults } from 'lib/utils/search';
-import { CATEGORY_CATEGORIES, CATEGORY_SEARCH } from 'lib/utils/analyticsConstants';
+import {
+  CATEGORY_CATEGORIES,
+  CATEGORY_SEARCH,
+  FEEDBACK_SEARCH
+} from 'lib/utils/analyticsConstants';
 
 const Services = ({
   categorisedResources,
@@ -27,6 +32,8 @@ const Services = ({
   const [openReferralForm, setOpenReferralForm] = useState({});
   const [referralData, setReferralData] = useState({});
   const [filteredResources, setFilteredResources] = useState(null);
+  const [feedbackSubmitted, setFeedbackSubmitted] = useState(false);
+  const [resultsTitle, setResultsTitle] = useState(null);
 
   const detailsClicked = (e, id, serviceId, categoryName) => {
     e.preventDefault();
@@ -37,6 +44,20 @@ const Services = ({
       setOpenReferralForm({ id: serviceId, categoryName });
     } else {
       setOpenReferralForm({});
+    }
+  };
+
+  const sendFeedback = e => {
+    let enteredText = e.target['search-feedback'].value;
+    if (enteredText?.length > 0) {
+      sendDataToAnalytics({
+        action: getUserGroup(referrerData['user-groups']),
+        category: FEEDBACK_SEARCH,
+        label: resultsTitle,
+        value: filteredResources?.resources?.length,
+        custom_text: enteredText
+      });
+      setFeedbackSubmitted(true);
     }
   };
 
@@ -62,11 +83,13 @@ const Services = ({
 
   const handleSearch = e => {
     e.preventDefault();
+    setFeedbackSubmitted(false);
 
     const searchTerm = e.target['search-input'].value;
+    setResultsTitle(searchTerm);
+
     const searchResults = getSearchResults(searchTerm, categorisedResources);
     const newFilteredResources = flattenSearchResults(searchResults);
-
     newFilteredResources.resources.sort((a, b) => b.weight - a.weight);
 
     setFilteredResources(newFilteredResources);
@@ -80,8 +103,13 @@ const Services = ({
   };
   const clickCategory = e => {
     document.getElementsByName('refer-details').forEach(x => x.removeAttribute('open'));
+
+    setResultsTitle(e);
     setOpenReferralForm({});
+    setFeedbackSubmitted(false);
+
     window.location.href = '#search-results-divider';
+
     sendDataToAnalytics({
       action: getUserGroup(referrerData['user-groups']),
       category: CATEGORY_CATEGORIES,
@@ -137,6 +165,36 @@ const Services = ({
               className={`${styles['search-results-header']}`}>
               {filteredResources.name}
             </h2>
+
+            <div className="govuk-!-margin-bottom-6">
+              <p>
+                If the results don't contain a service or information you require, please let us
+                know.
+              </p>
+              {!feedbackSubmitted ? (
+                <Details id="feedback-summary" title="Provide feedback">
+                  <form id="search-feedback-form" onSubmit={sendFeedback}>
+                    <div className="govuk-inset-text">
+                      <textarea
+                        id="search-feedback"
+                        name="search-feedback"
+                        maxLength="450"
+                        className={styles['feedback-textarea']}
+                      />
+                      <br />
+                      <button type="submit" className={`govuk-button`}>
+                        Send
+                      </button>
+                    </div>
+                  </form>
+                </Details>
+              ) : (
+                <div className={styles['success-message']}>
+                  Feedback submitted. Thank you for providing us with information to assist in
+                  improving this tool.
+                </div>
+              )}
+            </div>
 
             {filteredResources.resources.map(resource => (
               <ResourceCard
