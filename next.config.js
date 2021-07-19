54707d440c30f86ffe0a6301a3e0301075ea9dff
Kat <katrina@madetech.com>
2021-07-19 12:14:33 +0100

    Hardcode generic number

--- next.config.js
+++ next.config.js
@@ -6,7 +6,6 @@ module.exports = {
   env: {
     ADDRESSES_API_URL: process.env.ADDRESSES_API_URL,
     ADDRESSES_API_KEY: process.env.ADDRESSES_API_KEY,
-    GENERIC_CONTACT_NUMBER: process.env.GENERIC_CONTACT_NUMBER,
     HOTJAR_ID: process.env.HOTJAR_ID,
     OPTIMIZE_ID: process.env.OPTIMIZE_ID,
     EXTERNAL_USER_GROUP: process.env.EXTERNAL_USER_GROUP,
