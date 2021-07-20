4150cb0354520a0a82a9c417fd7374f4a047bdf7
Kat <katrina@madetech.com>
2021-07-20 14:01:23 +0100

    display most recent referral status

--- pages/referrals/status/[id].js
+++ pages/referrals/status/[id].js
@@ -1,9 +1,22 @@
 import { useState, useEffect } from 'react';
 import { requestByLinkId } from 'lib/api';
 import HttpStatusError from 'lib/api/domain/HttpStatusError';
+import { REFERRAL_STATUSES } from 'lib/utils/constants';
 
 const StatusHistory = ({ referral }) => {
-  return <div>{JSON.stringify(referral)}</div>;
+  const maxDate = new Date(Math.max(...referral.statusHistory.map(e => new Date(e.date))));
+  const recentStatus = referral.statusHistory.filter(status => new Date(status.date) != maxDate)[0];
+
+  return (
+    <div>
+      {recentStatus.status == REFERRAL_STATUSES.Approved && (
+        <div>The referral was approved on {recentStatus.date}</div>
+      )}
+      {recentStatus.status != REFERRAL_STATUSES.Approved && (
+        <div>The status of the referral is {recentStatus.status}</div>
+      )}
+    </div>
+  );
 };
 
 StatusHistory.getInitialProps = async ({ query: { id }, req: { headers }, res }) => {
