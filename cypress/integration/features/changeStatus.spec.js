a3d4039783725a2a5ccde3fb056fe34db0df7755
Kat <katrina@madetech.com>
2021-07-21 15:51:06 +0100

    reduce dynamodb test records by reseting status after tests

--- cypress/integration/features/changeStatus.spec.js
+++ cypress/integration/features/changeStatus.spec.js
@@ -6,7 +6,7 @@ context('status page', () => {
 
   describe('Accept referral', () => {
     it('shows status form', () => {
-      cy.visit('/referrals/status/5');
+      cy.visit('/referrals/status/1');
       cy.get('[data-testid=status-form-accepted-radio-item]').should('contain', 'Yes');
       cy.get('[data-testid=status-form-accepted-input]').click();
       cy.get('[data-testid=status-confirmation-panel]').should('not.exist');
@@ -18,15 +18,21 @@ context('status page', () => {
         'contain',
         'Your decision on this referral has been sent'
       );
-      cy.visit('/referrals/status/5');
+      cy.visit('/referrals/status/1');
 
       cy.get('[data-testid=status-paragraph]').should('contain', 'This referral was ACCEPTED on');
+      cy.resetReferralStatus('11', [
+        {
+          date: '2021-07-20T09:17:23.305Z',
+          status: 'SENT'
+        }
+      ]);
     });
   });
 
   describe('Reject referral', () => {
     it('with comment', () => {
-      cy.visit('/referrals/status/6');
+      cy.visit('/referrals/status/1');
       cy.get('[data-testid=status-form-rejected-radio-item]').should('contain', 'No');
       cy.get('[data-testid=status-form-rejected-input]').click();
       cy.get('[data-testid=status-form-rejected-comment-input]').type('comment');
@@ -40,10 +46,16 @@ context('status page', () => {
         'Your decision on this referral has been sent'
       );
 
-      cy.visit('/referrals/status/6');
+      cy.visit('/referrals/status/1');
 
       cy.get('[data-testid=status-paragraph]').should('contain', 'This referral was REJECTED on');
       cy.get('[data-testid=status-paragraph]').should('contain', 'with comment "comment".');
+      cy.resetReferralStatus('11', [
+        {
+          date: '2021-07-20T09:17:23.305Z',
+          status: 'SENT'
+        }
+      ]);
     });
   });
 });
