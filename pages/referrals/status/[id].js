e4827893229ab4303bb1b518ab7d33a0cf2b8b6e
Kat <katrina@madetech.com>
2021-07-22 09:36:44 +0100

    Add analytics for status update

--- pages/referrals/status/[id].js
+++ pages/referrals/status/[id].js
@@ -1,12 +1,13 @@
 import { requestByLinkId } from 'lib/api';
 import HttpStatusError from 'lib/api/domain/HttpStatusError';
-import { REFERRAL_STATUSES } from 'lib/utils/constants';
+import { REFERRAL_STATUSES, STATUS_UPDATE } from 'lib/utils/constants';
 import useReferral from 'lib/api/utils/useReferral';
 import { IsoDateTime } from 'lib/domain/isodate';
 import { useState } from 'react';
 import StatusForm from 'components/Feature/StatusForm';
 import Head from 'next/head';
 import { convertIsoDateToDateTimeString } from 'lib/utils/date';
+import { sendDataToAnalytics } from 'lib/utils/analytics';
 
 const StatusHistory = ({ referral }) => {
   const { updateReferral } = useReferral(null, {});
@@ -36,6 +37,12 @@ const StatusHistory = ({ referral }) => {
 
     await updateReferral(newReferral);
     setSubmitted(true);
+
+    sendDataToAnalytics({
+      action: newStatus,
+      category: STATUS_UPDATE,
+      label: initialReferral.service.name
+    });
   };
 
   return (
