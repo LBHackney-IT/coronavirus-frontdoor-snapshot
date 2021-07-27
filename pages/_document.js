4228d5df1a564978db38c1c21d7b85f49ca933d8
Owen Pigott <owen.pigott@madetech.com>
2021-07-27 13:01:31 +0100

    Remove hotjar references

--- pages/_document.js
+++ pages/_document.js
@@ -6,20 +6,6 @@ export default class AppDocument extends Document {
       <Html lang="en-GB" className="govuk-template lbh-template">
         <Head>
           <script
-            dangerouslySetInnerHTML={{
-              __html: `
-            (function(h,o,t,j,a,r){
-              h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
-              h._hjSettings={hjid:${process.env.HOTJAR_ID},hjsv:6};
-              a=o.getElementsByTagName('head')[0];
-              r=o.createElement('script');r.async=1;
-              r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
-              a.appendChild(r);
-             })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
-          `
-            }}
-          />
-          <script
             src={`https://www.googleoptimize.com/optimize.js?id=${process.env.OPTIMIZE_ID}`}></script>
           <script
             async
